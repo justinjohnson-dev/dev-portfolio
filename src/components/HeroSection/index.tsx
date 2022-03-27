d9f5ad5d139eec746267eedd4853597d17982cfc
Justin Johnson <jjustin634@gmail.com>
2022-03-27 16:36:01 -0500

    adding initial heroContent infortion

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
+import React, { useState } from 'react';
+import {
+  ArrowForward,
+  ArrowRight,
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+} from './HeroElements';
+import { Button } from '../ButtonElement';
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
-    <HeroContainer id='home'>
+    <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src='/video.mp4' />
       </HeroBg>
+      {/* <HeroContent>
+        <HeroH1>@ Justin Johnson</HeroH1>
+        <HeroP>Software Engineer | Data Engineer | Computer Enthusiast</HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to=''
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary='true'
+            dark='true'
+          >
+            Dive in {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent> */}
     </HeroContainer>
   );
 };

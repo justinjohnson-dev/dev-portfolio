0d62d551ad3738ebb88d9cdc5636aed6e3c6f7d7
Justin Johnson <jjustin634@gmail.com>
2022-04-09 12:20:31 -0500

    adding components building react scroll to navbar (#5)

---
+++ src/components/Footer/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+} from './FooterElements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle> About JJ </FooterLinkTitle>
+              <FooterLink to='about'>Intro</FooterLink>
+              <FooterLink to='/api/graphql'>
+                Want to test the playground?
+              </FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Footer from '../components/Footer';
 import HeroSection from '../components/HeroSection/';
 import InfoSection from '../components/InfoSection/';
 import { HomeObjOne } from '../components/InfoSection/Data';
@@ -26,6 +27,7 @@ const HomePage = () => {
         <>
           <NavBar toggle={toggle} />
           <InfoSection {...HomeObjOne} />
+          <Footer />
         </>
       )}
       <SideBar
